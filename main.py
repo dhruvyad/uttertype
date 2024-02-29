bc8cc814fbd48fb57c01ff6ad25048b90fca2835
Dhruv Yadav <dhruv1103@gmail.com>
2024-02-28 23:55:37 -0800

    Add dotenv

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import threading
 from transcriber import AudioTranscriber
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()
     transcriber = AudioTranscriber()
     t = threading.Thread(target=transcriber.listen_for_keypress)
     t.start()
